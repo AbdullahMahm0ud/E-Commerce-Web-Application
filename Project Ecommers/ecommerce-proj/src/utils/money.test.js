7c5ca76c408f2d8349bd353157bb06d5d10dd99c
AbdullahMahm0ud <abdullahbusinessme@gmail.com>
2026-07-07 21:22:48 +0300

    added a test and fix for negative numbers

--- Project Ecommers/ecommerce-proj/src/utils/money.test.js
+++ Project Ecommers/ecommerce-proj/src/utils/money.test.js
@@ -10,4 +10,16 @@ describe("formatMoney", () => {
     expect(formatMoney(1090)).toBe("$10.90");
     expect(formatMoney(100)).toBe("$1.00");
   });
+
+  it("formats 0 dollars into $0.00", () => {
+    expect(formatMoney(0)).toBe("$0.00")
+  })
+
+  it("formats negative numbers properly", () => {
+    expect(formatMoney("-100")).toBe("-$1.00")
+  })
+
+  it("formats negative numbers properly", () => {
+    expect(formatMoney("-999")).toBe("-$9.99")
+  })
 });
